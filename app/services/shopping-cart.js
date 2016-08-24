2fbea59b5d3ffb59830026bfd23f6d9f90ccdfbe
Ben Logue and Joel Delight <joel@>
2016-08-24 11:00:28 -0700

    add cartTotal to shopping-cart service.

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,9 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  cartTotal2: 0,
 
   add(item) {
-
     this.get('items').pushObject(item);
-  }
+  },
+  cartTotal: Ember.computed('items.length', function() {
+    var runningTotal = 0;
+    this.get("items").forEach(function(item) {
+      runningTotal += item.get('cost');
+    });
+    console.log(runningTotal);
+    return runningTotal;
+  }),
 });
