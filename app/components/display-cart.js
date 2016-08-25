fee198abc69c59f8226b75737e732750f70866d2
Ben Logue and Joel Delight <ben@>
2016-08-25 11:16:22 -0700

    finish cart checkout.

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     shoppingCart: Ember.inject.service(),
+    actions: {
+      checkout() {
+        if (confirm('Buy these items for the stated price?')) {
+          var items = this.get('shoppingCart.uniqueItems');
+          var quantities = this.get('shoppingCart.uniqueItemsCount');
+          for(var i = 0; i < items.length; i ++) {
+            var workingItem = items[i];
+            for (var j = 0; j < this.get('products.length'); j ++) {
+              var checkedProduct = this.get('products').toArray()[j];
+              if(workingItem === checkedProduct) {
+                checkedProduct.set('quantity', (checkedProduct.get('quantity') - quantities[i]));
+                checkedProduct.save();
+              }
+            }
+          }
+          this.set('shoppingCart.uniqueItems', []);
+          this.set('shoppingCart.uniqueItemsCount', []);
+          this.set('shoppingCart.items', []);
+        }
+      },
+      nextAction() {
+
+      },
+    }
 });
--- app/helpers/date-display.js
+++ app/helpers/date-display.js
@@ -3,7 +3,6 @@ import moment from 'moment';
 
 export function dateDisplay(params) {
   var timestamp = params[0] / 1000;
-  console.log("Hi, in date display-  " + timestamp);
   var date = moment.unix(timestamp).format("MM-DD-YYYY h:mm a");
   return date;
 }
