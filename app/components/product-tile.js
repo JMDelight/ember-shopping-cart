d8c57adb5db3dd8fcbf12f4860f104c35938534f
Ben Logue and Joel Delight <ben+joel@>
2016-08-25 09:11:40 -0700

    added a computed property to only temporarily remove items from inventroy when they are in a cart.

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -2,15 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  testingItem: 0,
   actions: {
     addToCart(item) {
       this.get('shoppingCart').add(item);
-      var product = this.get('product');
-      product.set('quantity', product.get('quantity') - 1);
-      product.save();
+      // this.set("testingItem", this.get('testingItem') + 1);
+      // console.log("hi ", this.get('testingItem'));
+      // var product = this.get('product');
+      // product.set('quantity', product.get('quantity') + 1);
+      // product.save();
     },
     save(params) {
       this.sendAction('save', params);
-    }
-  }
+    },
+  },
+  availableItems: Ember.computed('shoppingCart.items.length', function() {
+    console.log("working");
+    var numberInCart = 0;
+    var currentItem = this.get('product');
+    console.log(currentItem.get('quantity'));
+    this.get('shoppingCart.items').forEach(function(item) {
+      if (item === currentItem) {
+        numberInCart ++;
+      }
+    });
+    return (currentItem.get('quantity') - numberInCart);
+  }),
 });
